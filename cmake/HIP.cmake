f4f421b06e7bdb579a7bd8bf328cc6150bdac056
german-v-t-um-es <german.v.t@um.es>
2025-01-14 20:57:33 +0100

    Compatible version with gem5 docker container for gpu-se

---
+++ cmake/HIP.cmake
@@ -0,0 +1,15 @@
+if (HMARK_BUILD_HIP)
+	set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} /opt/rocm/hip/cmake)
+	include(FindHIP)
+  add_definitions(-DHMARK_BUILD_HIP=1 -DCOMPILE_HSA=1)
+
+	if (HIP_FOUND)
+		message("-- hipcc found, will compile hip benchmarks.")
+		include_directories("/opt/rocm/hip/include")
+		set(HIP_HIPCC_FLAGS ${HIP_HIPCC_FLAGS} -std=c++11)
+	else (HIP_FOUND)
+		message("--hip not found")
+		set(HMARK_BUILD_HIP Off)
+	endif(HIP_FOUND)
+
+endif (HMARK_BUILD_HIP)
